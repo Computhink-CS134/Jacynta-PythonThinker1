e5bd30dfb9cab88210354436d1b8df7b3ab4b38c
Computhink-CS134 <cs134@computhink.com.sg>
2026-01-08 11:00:45 +0000

    lesson 1

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,55 @@
-print("Hello from lesson 2")
+# Recap 1
+ 
+# print("Red")
+# print("Orange")
+# print("Yellow")
+# print("Green")
+# print("Blue")
+# print("Indigo")
+# print("Violet")
 
-######## Write the pseudocode in comments for task 2 here
-# Using comments, translate the code shown on screen into pseudocode.
+## task 1a
+# say 'hey!'
+# move 10 steps 
+# Repeat 10 times
 
-######## Write the pseudocode in comments for task 3 here
-# Using comments, translate the code shown on screen into pseudocode.
+## task 1b
+# counter = 0
+# move 10 steps
+# turn 15 degrees
+# change counter by 10
+# repeat until counter = 50
+
+## task 1c
+# ask "what is your age?"
+# if age (ans) = less than 18
+# say "access denied"
+# else say "welcome!"
+
+# task 2
+# create a glass, plastic, and paper bin(variables)
+# get items made of glass, plastic, or paper
+# check what if item is made of glass
+# if item is glass, then put item in the glass bin
+# else if not glass, then check if item is plastic
+# if item is plastic, then put item in the plastic bin
+# else put item in the paper bin
+# repeat until items are finished
+
+# task 3
+# create a secret code
+# ask user for the scret code
+# if user enters wrongly, say "error! that is incorrect."
+# if user enters correctly, say "congratulations! that is correct"
+
+# task 4
+# get student's test 1-3 scores
+# make students test scores a variable(3)
+# make max test scores a variable(3)
+# (test1 maxscore/test1 x 20%)+(test2 maxscore/test2 x 40%)+(test3 maxscore/test3 x 40%)= final score
+
+#  task 4
+# test 1 = test 1 x 0.20
+# test 2 = test 2 x 0.40
+# test 3 = test 3 x 0.40
+# students final score = test 1 + test 2 + test 3
