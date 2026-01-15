29eee39f190a46c400dfe90d2514185c728ca8a5
Computhink-CS134 <cs134@computhink.com.sg>
2026-01-15 11:05:32 +0000

    Lesson 2

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,102 @@
-print("Hello from lesson 3")
+import math 
+
+
+# my_string = "Jacynta"
+# print(my_string)
+# my_num = "1181214"
+# print(my_num)
+
+# task 1a
+# age = 12
+# print(age)
+
+# task 1b 
+# x = 10
+# x = 20
+# print(x)
+
+# task 2
+# variable1 = 3
+# variable2 = 5
+# print(variable1 + variable2) 
+
+# task 2b 
+# variable1 = 3
+# variable2 = 5
+# print(variable1 * variable2)
+# print(variable1 / variable2)
+# print(variable1 - variable2)
+
+# task 3a
+# print(round(42.567899, 2))
+
+# task 3b
+# print(round(1267.15))
+
+# task 3c
+# print(round(1785.956,-2))
+# -1 = tens
+# -2 = hundreds
+# -3 = thousands....etc
+
+# task 4a 
+# a = 8
+# b= 12
+# print(a + b)
+
+# task 4b 
+# num1 = 50
+# num2 = 10
+# ans = num1 - num2
+# print(ans * 3)
+
+# task 4c 
+# num1 = 100 / 4 
+# print(num1)
+
+# task 4d
+# ans = ((3 + 4) * 5 - (10 / 2))
+# print(ans)
+
+# task 5a
+# pt = 15 * 2
+# bk = 58
+# print(round(pt + bk , 2))
+
+# task 5b
+# sp = 96
+# sd = 18 * 3
+# br = 2 * 6
+# total = (sp + sd + br) * 1.1 - 25
+# print(round(total, 2))
+
+# task 5c 
+# sp = 100 * 5
+# sd = 20 * 8
+# br = 2.5 * 20
+# total = (sp + sd + br) * 1.1 * 1.09
+# print(round(total, 2))
+
+# task 6a
+# print(abs(-55))
+
+# task 6b
+# print(abs(-8))
+
+# task 7a
+# kg = math.floor(9.8)
+# ans = kg / 1
+# print(ans)
+
+# task 7b
+# bags = 27 / 4
+# total = math.ceil(bags)
+# print(total)
+
+# task 8a
+# cir = math.pi * 100
+# print(round(cir, 2))
+
+# task 8b
+# cir2 = math.pi * 12 ** 2
+# print(round(cir2, 2))
