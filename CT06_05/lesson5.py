8e9960a063a511978490a8c90502eb670ba251de
Computhink-CS134 <cs134@computhink.com.sg>
2026-01-29 11:05:48 +0000

    lesson 4

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,82 @@
-print("Hello from lesson 5")
+# recap 1
+# name = input("What is your name? ")
+# age = input("How old are you? ")
+# p_msg = input("Type your personal message here: ")
+
+# sentence = f"Happy {age}th birthday {name}! {p_msg}"
+# print(sentence)
+
+#  task 1
+# for i in range(100):
+#     print("I like chicken rice")
+
+# #  task 2
+# for i in range(100):
+#     print("I like cake")
+#     print("Give me more")
+
+# task 3
+# for i in range(0, 60, 1):
+#     print(i)
+
+# task 4a
+# for i in range(1, 6, 1):
+#     print(i)
+
+#  task 4b
+# for i in range(51, 101, 1): 
+#     print(i)
+
+# task 4c
+# for i in range(18, 30, 1):
+#     print(i)
+
+#  task 5a 
+# for i in range(2, 25, 2):
+#     print(i)
+
+# task 5b
+# for i in range(8, 97, 8):
+#     print(i)
+
+# task 5c
+# for i in range(5, 0, -1):
+#     print(i)
+
+#  task 6a 
+# print("Ready!")
+# for i in range(3, 0, -1):
+#     print(i)
+# print("Go!")
+
+# task 6b
+# for i in range(10, 0, -1):
+#     print(i)
+# print("Boo!")
+
+# task 7
+# start = input("Type a starting number for the loop: ")
+# start = int(start)
+# stop = input("Type a ending number for the loop: ")
+# stop = int(stop)
+
+# for i in range(start, stop + 1, 1):
+#     print(i)
+
+#  task 8a
+# total = 0
+# for i in range(2, 41, 2):
+#     total = total + i
+# print(total)
+
+# task 8b
+# total = 0
+# for i in range(9, 100, 2):
+#     total = total + i
+# print(total)
+
+# task 9
+# word = input("What is your name? ")
+# for char in word:
+#     print(f"give me a {char} !")
+
