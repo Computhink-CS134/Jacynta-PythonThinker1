44379b265370c37cb39a3ed0d049700b0b8ffcb8
Computhink-CS134 <cs134@computhink.com.sg>
2026-02-05 11:03:12 +0000

    lesson 5

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,78 @@
-print("Hello from lesson 6")
+# Recap 1
+
+# total_students = int(input("How many students are there in the class? "))
+# total_score = 0
+
+# for i in range(1, total_students + 1, 1):
+#     score = input(f"What is student {i}'s score? ")
+#     total_score = total_score + int(score)
+
+# print(f"Total score = {total_score}")
+# average = total_score/total_students
+# print(f"Average = {average}")
+
+# **Task 1a**:
+# for i in range(3):
+#     print("Hello, World!")
+
+# **Task 1b**:
+# for i in range(5):
+#     print(i)
+
+# **Task 1c**:
+# print("Hello, World!")
+
+# **Task 1d**:
+# fi = 5
+
+# **Task 1e**:
+# print ("Hello, World!")
+
+# ------------------------------
+
+# **Task 2a**:
+# age = 12
+# print(age)
+
+# **Task 2b**:
+# name = "Alice"
+# print(name)
+
+# **Task 2c**:
+# x = 5
+# print(x)
+
+
+# **Task 2d**:
+# print("Hello, World!")
+
+# ---------------------------------------------------------------
+
+# ## Task 3: Type Errors
+# Fix the errors in the following:
+
+# **Task 3a**:
+# age = 25
+# print(age + 1)
+
+# **Task 3b**:
+# number = 10
+# print(number - 5)
+
+# **Task 3c**:
+# Repeat = 3
+# print(Repeat * 3)
+
+# **Task 3d**:
+# year = 2023
+# print(f"The year is {year}")
+
+# **Task 3e**:
+# x = 10
+# y = x / 2
+
+# **Task 3f**:
+# end = 5
+# for i in range(end):
+#     print(i)
+
--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,103 @@
-print("Hello from lesson 7")
+# Recap 1
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print(f"Average score for {student_name} is {average_score}")
+
+#  Task 1
+
+# for i in range(1, 11):
+#     print(i)
+
+
+# Task 2
+
+# for i in range(2, 21, 2):
+#     print(i)
+
+
+# Task 3 
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+# Task 4
+
+# word = input("What word would you like to repeat? ")
+# n = int(input("How many times would you like to repeat? "))
+
+# for i in range(n):
+#     print(word)
+
+
+# task 5
+
+# name = input("What is your name? ")
+# n = int(input("How many times would you like to repeat it? "))
+# sentence = f"Nice to meet you, {name}."
+
+# for i in range(n):
+#     print(sentence)
+
+#  task 6
+
+# total = 0
+
+# for i in range(5):
+#     number = int(input(f"What is number {i + 1}? "))
+#     total = total + number
+
+# print(total)
+
+# Task 7
+
+# number = int(input("What number for the timetable? "))
+
+# for i in range(1, 13):
+#     ans = number * i
+#     print(f"{number} x {i} = {ans}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
