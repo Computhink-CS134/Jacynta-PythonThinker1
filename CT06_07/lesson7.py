8437a861a1dc8954ade006346faf81b377380f29
Computhink-CS134 <cs134@computhink.com.sg>
2026-02-12 11:04:26 +0000

    lesson 6

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -67,6 +67,68 @@
 #     print(f"{number} x {i} = {ans}") 
 
 
+# Task 8
+
+# number = int(input("Type a number: "))
+# number = number + 1
+
+# for i in range(1, number):
+#     print(str(i) * i)
+
+
+#  Task 9
+
+# total = 0
+
+# for i in range(1, 5 + 1):
+#     score = int(input(f"What is student {i}'s score? "))
+#     total = score + total
+
+# average = total / 5
+# print(average)
+
+
+# Task 10
+
+# students = int(input("How many students are there? "))
+# students = students + 1
+# total = 0
+
+# for i in range(1, students):
+#     score = int(input(f"What is student {i}'s score? "))
+#     total = score + total
+
+# average = total / students
+# print(round(average))
+
+
+# Task 11
+
+# number = int(input("Type a number: "))
+
+# for i in range(1, number + 1):
+#     print("#" * i)
+
+# for i in range(number - 1, 1, -1):
+#     print("#" * i)
+
+
+# Task 12
+
+# number = int(input("Type a number: "))
+
+# for i in range(1, number + 1):
+#     spaces = number - i
+#     print(f"{" " * spaces}{"#" * i}")
+
+# for i in range(number - 1, 1, -1):
+#     spaces = " " * (int(i) - 1)
+#     print(f"{spaces}{"#" * i}")
+
+   
+
+
+
 
 
 
